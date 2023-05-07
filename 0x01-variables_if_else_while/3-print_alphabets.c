3164e84f20dd101edfdad1cfefaf6147a054c747
Saraah <Saraahamrr98@gmail.com>
2023-05-07 09:24:41 -0700

    zz

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdtio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch1 = 'a';
+
+	while (ch1 <= 'Z')
+	{
+		putchar('ch1');
+		ch1++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
