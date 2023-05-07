22df96d4646120d3da732438c538c87c77513908
Saraah <Saraahamrr98@gmail.com>
2023-05-07 11:13:16 -0700

    charZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
