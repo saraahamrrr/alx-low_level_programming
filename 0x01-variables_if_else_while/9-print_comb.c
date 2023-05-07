ad12bf59020412af1b3c8d0b6182b73bd936c72f
Saraah <Saraahamrr98@gmail.com>
2023-05-07 12:02:54 -0700

    charZ

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		if (num != 9)
+			putchar(',');
+			putchar(',');
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
