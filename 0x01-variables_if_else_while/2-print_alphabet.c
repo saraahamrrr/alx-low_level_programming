0a120dac2e3df8aaccd75e37001a8ab2e9b9591d
Saraah <Saraahamrr98@gmail.com>
2023-05-07 08:59:19 -0700

    chR

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdtio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n = 97;
+
+	while (n <= 122)
+{
+	putchar(n);
+	n++;
+}
+	putchar("\n");
+
+	return (0);
+}
