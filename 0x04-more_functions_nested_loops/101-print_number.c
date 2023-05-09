f3d5dcb94d17695fc13a1b7d32720516dd0ec79e
Saraah <Saraahamrr98@gmail.com>
2023-05-09 11:50:56 -0700

    charZ

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,13 +13,13 @@ void print_number(int n)
 	/*first check if its negative*/
 	if (n < 0)
 	{
-	_putchar('-');
-	num = -num;
+		_putchar('-');
+		num = -num;
 	}
 
 	/*print the first few digits*/
 	if ((num / 10) > 0)
-	print_number(num / 10);
+		print_number(num / 10);
 
 	/*print the last digit*/
 	_putchar((num % 10) + 48);
